b69f10b04bb2f993d1e2daadee7ffc1463ab78a0
Trina <92634994+trinayau@users.noreply.github.com>
2022-09-21 11:26:22 +0100

    added code

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,35 @@
+import javax.swing.*;
+import java.awt.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        JFrame application = createGUI();
+        application.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        application.setVisible(true);
+    }
+
+    private static JFrame createGUI(){
+        JTextField input = new JTextField();
+        input.setPreferredSize(new Dimension(300, 40 ));
+
+        JButton convertButton = new JButton("Convert");
+
+        JLabel output = new JLabel();
+        output.setPreferredSize(new Dimension (300, 40));
+
+        convertButton.addActionListener(event -> {
+            output.setText(input.getText());
+        });
+
+        JFrame gui = new JFrame("Title Case Converter");
+        gui.setLayout(new FlowLayout());
+        gui.add(input);
+        gui.add(convertButton);
+        gui.add(output);
+        gui.pack();
+        gui.setLocationRelativeTo(null); //centering screen
+
+        return gui;
     }
 }
