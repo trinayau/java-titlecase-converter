70a851a51652555144af541b477b8d2e604192b0
Trina <92634994+trinayau@users.noreply.github.com>
2022-09-21 11:31:34 +0100

    added code to titlecaseconverter class

---
+++ src/TitleCaseConverter.java
@@ -0,0 +1,16 @@
+public class TitleCaseConverter {
+    public static String convertToTitleCase(String input){
+        StringBuilder builder = new StringBuilder();
+
+        for(String word:  input.split("\\s")){
+            if (word.length() < 4) {
+                builder.append(word.toLowerCase());
+            } else {
+                builder.append(word.substring(0,1).toUpperCase());
+                builder.append(word.substring(1).toLowerCase());
+            }
+            builder.append(" ");
+        }
+        return builder.toString();
+    }
+}
